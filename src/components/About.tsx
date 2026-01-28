a7b0ce88e252d9e59024eecaff118ab8ed0f79dc
FaureGalliard <angelcrispinvaldivia@gmail.com>
2026-01-27 22:52:44 -0500

    feat(portfoio: add explorevalley project section with images)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,10 @@ export default function About() {
   return (
     <section id="about" className="py-24 px-6 bg-[rgba(16,17,17,1)] ">
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-3xl font-semibold">Sobre mí</h2>
+        <h2 className="text-3xl font-semibold">Sobre mi</h2>
 
         <p className="mt-6 text-zinc-400 max-w-3xl leading-relaxed">
-          Desarrollo sistemas y aplicaciones con enfoque en lógica, arquitectura y eficiencia.
+          Ingeniero de Software orientado a la automatización y a construir soluciones funcionales desde cero.
         </p>
       </div>
     </section>
